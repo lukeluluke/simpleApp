7c6e524a9eab86d29216eba2cdf41b374950e141
lukeluluke <luke0427@hotmail.com>
2019-01-31 10:12:25 +1100

    Create a simple app to test memory leaks with SDK v7.5.0

---
+++ SimpleProject2/app/controllers/index.js
@@ -0,0 +1,31 @@
+var count = 0;
+
+function searchClick(e) {
+	console.log('Start to make api call');
+	var url = 'https://pokeapi.co/api/v2/ability/4/';
+	var xhr = Ti.Network.createHTTPClient({
+		onload: function(e) {
+			Ti.API.debug(this.responseText);
+			// alert('success');
+			console.log('Success');
+			count ++;
+			$.num.text = count + ' calls';
+		},
+		onerror: function(e) {
+			Ti.API.debug(e.error);
+			console.log('error');
+			// alert('error');
+		},
+		timeout:5000
+	});
+	xhr.open("GET", url);
+	xhr.send();
+}
+
+
+
+setInterval(function () {
+	searchClick();
+}, 10000)
+
+$.index.open();
